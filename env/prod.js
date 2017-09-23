24ebd9ea6b922d82937b3029856b37d3130fd26f
wangcq <wangcq@linkface.cn>
2017-09-23 23:39:17 +0800

    update env

--- env/prod.js
+++ env/prod.js
@@ -1,6 +1,6 @@
 const envify = require('./envify');
 module.exports = {
 	NODE_ENV: 'production',
-	BAIDU_MAP_URL: 'http://j.map.baidu.com/g50PL',
+	BAIDU_MAP_URL: 'http://j.map.baidu.com/Q_EZL',
 	CDN_PREFIX: 'http://wungcqrain.oss-cn-beijing.aliyuncs.com'
 }
