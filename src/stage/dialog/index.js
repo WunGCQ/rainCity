d9125c708d17eced31a956a4d80adf4e2dd2d180
wangcq <wangcq@linkface.cn>
2017-09-23 20:01:44 +0800

    update lockMove

--- src/stage/dialog/index.js
+++ src/stage/dialog/index.js
@@ -1,17 +1,19 @@
-import { templateStr,emptyTemplateStr } from './template';
+import { templateStr, emptyTemplateStr } from './template';
 import { Visualizer } from './Visualizer';
 import { getState } from '../../state';
-import {load} from './Player/load';
-import {iOSLoad,clearContext} from './Player/iOSLoad';
-import {rainDrops} from '../scene/drops/dropTypes';
+import { load } from './Player/load';
+import { iOSLoad, clearContext } from './Player/iOSLoad';
+import { rainDrops } from '../scene/drops/dropTypes';
+
 const state = getState();
 const isiOS = !!navigator.userAgent.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/);
 //const loadFunc = isiOS ? iOSLoad : load;
 const loadFunc = iOSLoad;
 const root = $('#root');
+
 function insertDialog(index) {
 	const data = rainDrops[index]; //点击水滴的信息
-	const domString = _.template(data.last? emptyTemplateStr : templateStr)({
+	const domString = _.template(data.last ? emptyTemplateStr : templateStr)({
 		index: index,
 		audio: '',
 		position: data.position, //地点
@@ -49,41 +51,39 @@ export const showDialog = (function () {
 			return;
 		}
 		insertDialog(i);
+		getState().haveOpening = true;
+		me && (me.lockMove = true);
 
 		const el       = $('#dialog_' + i),
 		      content  = $('.m-dialog-content-text', el),
 		      closeBtn = $('.m-close-btn', el);
-        let showQR = false;
-        closeBtn.on('touchstart', function (ev) {
-            ev && ev.preventDefault();
-            if (showQR) {
-                showQR = false;
-                $('.m-qr-block', el).removeClass('show');
-            } else {
-                clearContext();
-                me && (me.lockMove = false);
-	            getState().haveOpening= false;
-                el.removeClass('show');
-                el.remove();
-            }
-        });
-		if(rainDrops[i].last) {
+		let showQR = false;
+		closeBtn.on('touchstart', function (ev) {
+			ev && ev.preventDefault();
+			if (showQR) {
+				showQR = false;
+				$('.m-qr-block', el).removeClass('show');
+			} else {
+				clearContext();
+				me && (me.lockMove = false);
+				getState().haveOpening = false;
+				el.removeClass('show');
+				el.remove();
+			}
+		});
+		if (rainDrops[i].last) {
 			return;
 		}
 		const top = content.get(0).offsetTop, maxHeight = 430 - top - 10;
 		content.css('height', maxHeight + 'px').css('overflow', 'auto');
 		const btn = $('.m-play-btn', el).get(0), qrBtn = $('.m-share-qr-btn', el);
 		$('.m-share-tip-btn', el).on('touchstart', showShare);
-         showQR =  function () {
-                $('.m-qr-block', el).addClass('show');
-                showQR = true;
-            };
+		showQR = function () {
+			$('.m-qr-block', el).addClass('show');
+			showQR = true;
+		};
 
-		qrBtn.on('touchstart',showQR );
-
-		if (getState().haveOpening) {
-			return;
-		}
+		qrBtn.on('touchstart', showQR);
 		//el.on('touchstart',function(ev){
 		const content_el = content.get(0);
 		content.on('touchstart', function (ev) {
@@ -101,10 +101,10 @@ export const showDialog = (function () {
 			ev.stopPropagation();
 		});
 		el.addClass('show');
-		me && (me.lockMove = true);
+
 		const musicURL = `${process.env.CDN_PREFIX}/media/${rainDrops[i].file}.mp3`;
 		loadFunc(btn, musicURL);
-		getState().haveOpening = true;
+
 	};
 })();
 
